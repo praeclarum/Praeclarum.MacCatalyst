3955d5b8853dc3d6d1b4025dbaa6de6127e806da
Frank A. Krueger <fak@praeclarum.org>
2020-12-28 18:06:30 -0800

    Only run if input changes

--- test_build_task.sh
+++ test_build_task.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-set +x
-set +e
+set -x
+set -e
 
 
 PROJDIR=/Users/fak/Dropbox/Projects/Microcasts/Microcasts.iOS
 
 dotnet build Praeclarum.MacCatalyst/Praeclarum.MacCatalyst.csproj
-cp Praeclarum.MacCatalyst/bin/Debug/netstandard2.0/Praeclarum.MacCatalyst.dll $PROJDIR
+cp Praeclarum.MacCatalyst/bin/Debug/netstandard2.1/Praeclarum.MacCatalyst.dll $PROJDIR
 cp Praeclarum.MacCatalyst/Praeclarum.MacCatalyst.targets $PROJDIR
 
 cd $PROJDIR
+touch AppDelegate.cs
 msbuild
 
 
